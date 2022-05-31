99a91cdc6e0ad56056fbad2d603399ed4aa5e110
Mikkel Siercke <mikkelsiercke@outlook.com>
2022-05-31 21:33:32 +0200

    Project file

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', description: 'fhsjkdfh' },
+  { id: 12, name: 'Narco', description: 'Heloo' },
+  { id: 13, name: 'Bombasto', description: 'fhsjkdfh' },
+  { id: 14, name: 'Celeritas', description: 'fhsjkdfh' },
+  { id: 15, name: 'Magneta', description: 'fhsjkdfh' },
+  { id: 16, name: 'RubberMan', description: 'fhsjkdfh' },
+  { id: 17, name: 'Dynama', description: 'fhsjkdfh' },
+  { id: 18, name: 'Dr IQ', description: 'fhsjkdfh' },
+  { id: 19, name: 'Magma', description: 'fhsjkdfh' },
+  { id: 20, name: 'Tornado', description: 'fhsjkdfh' }
+];
